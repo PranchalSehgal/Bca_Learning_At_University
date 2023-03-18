b39eeea70249eb325927f1a14dc4054c0c69cee1
Pranchal <127619615+PranchalSehgal@users.noreply.github.com>
2023-03-18 13:57:10 +0530

    Update Assignment4.c

--- Assignment4.c
+++ Assignment4.c
@@ -2,6 +2,18 @@
 
 int main()
 {
-  
+  int main() {
+int matrix[3][3] = {{13, 4, 789}, {1, 66, 80}, {19, 176, 8}};
+int i, j, max;
+
+for (i = 0; i < 3; i++) {
+    max = matrix[i][0];
+    for (j = 1; j < 3; j++) {
+        if (matrix[i][j] > max) {
+            max = matrix[i][j];
+        }
+    }
+    printf("Maximum element in row %d is %d\n", i + 1, max);
+}
   return 0;
 }
