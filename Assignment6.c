d1848c0a9952c30d2cba9feeed07b8bbd275e4a9
Pranchal <127619615+PranchalSehgal@users.noreply.github.com>
2023-03-18 13:32:47 +0530

    Update Assignment6.c

--- Assignment6.c
+++ Assignment6.c
@@ -2,5 +2,25 @@
 
 int main()
 {
+  int arr[100], n, i, j, count = 1;
+
+printf("Enter the number of elements in the array: ");
+scanf("%d", &n);
+
+printf("Enter the elements of the array in sorted order:\n");
+for (i = 0; i < n; i++)
+{
+    scanf("%d", &arr[i]);
+}
+
+for (i = 0; i < n - 1; i++)
+{
+    if (arr[i] != arr[i+1])
+    {
+        count++;
+    }
+}
+
+printf("The number of distinct elements in the array is: %d", count);
   return 0;
 }
