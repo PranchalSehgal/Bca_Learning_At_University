6fecd75c22bee1508df98ff26059501c2d5d3190
Pranchal <127619615+PranchalSehgal@users.noreply.github.com>
2023-04-27 19:23:45 +0530

    Update Assignment10.c

--- Assignment10.c
+++ Assignment10.c
@@ -4,11 +4,38 @@
 //if both even and odd available it is known as MIXED array
 
 
-int findType(int n, int *) //complete this function
+int findType(int *array, int n) //complete this function
 {
-  
+  int oddCount = 0;
+    int evenCount = 0;
+
+    for(int i = 0; i < n; i++) {
+        if(array[i] % 2 == 0) {
+            evenCount++;
+        }
+        else {
+            oddCount++;
+        }
+    }
+
+    if(evenCount == n) {
+        printf("EVEN ARRAY\n");
+    }
+    else if(oddCount == n) {
+        printf("ODD ARRAY\n");
+    }
+    else {
+        printf("MIXED ARRAY\n");
+    }
 }
 int main()
 {
+  int array1[] = {1,3,5,7,9};
+    int array2[] = {2,4,6,8,10};
+    int array3[] = {1,2,3,4,5,6};
+
+    findType(array1, 5);
+    findType(array2, 5);
+    findType(array3, 6);
   return 0;
 }
